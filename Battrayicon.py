dcaacebbbdb150c8772b9a14fc12a82e453c3d8d
Umut Şen <umutsen2662@gmail.com>
2024-08-26 19:23:46 +0300

    changed the icon title to show my razer moice battery status

--- Battrayicon.py
+++ Battrayicon.py
@@ -1,44 +1,45 @@
 from PIL import Image, ImageDraw, ImageFont
-import psutil, time, json, os, Logs, Plot
+import psutil, time, json, os, Logs, Plot, Razer
 import pystray as ps
 
+FQ = 36000
 run = True
 log = False
 plot = False
 
 def main():
     battery_percent = psutil.sensors_battery().percent
-    battery_charging = psutil.sensors_battery().power_plugged
-    icon = ps.Icon(name="Battray", 
-                   title=("Charging" if battery_charging else "Discharging"), 
+    icon = ps.Icon(name="Battray",
                    icon=create_tray_image(str(battery_percent)), 
                    menu=ps.Menu(ps.MenuItem("Plot", on_clicked), 
                                 ps.MenuItem("Logs", on_clicked), 
                                 ps.MenuItem("Exit", on_clicked)))
     icon.run_detached()
+
+    repetitions = FQ
     global run, log, plot
     while (run):
-        if (psutil.sensors_battery().power_plugged != battery_charging):
-            battery_charging = not battery_charging
-            icon.title = ("Charging" if battery_charging else "Discharging")
-        
+        repetitions += 1
+        if (repetitions > FQ):
+            razer_battery = Razer.get_battery()
+            if razer_battery:
+                icon.title = f"Mouse {razer_battery}%"
+                log_data(battery_percent, razer_battery)
+            else:
+                icon.title = "Mouse not found"
+            repetitions = 0
+
         if plot:
             Plot.plot_graph()
             plot = False
         elif log:
             Logs.show_logs()
             log = False
-        else:
+        elif (repetitions % 50 == 0):
             battery_percent = update(icon, battery_percent)
-        
-        time.sleep(0.2)
-    return
 
-def create_tray_image(text):
-    image = Image.new('RGBA', (64, 64), (255, 0, 0, 0))
-    dc = ImageDraw.Draw(image)
-    dc.text((0,0), text, font=ImageFont.truetype("arial.ttf", 60), fill=(255, 255, 255))
-    return image
+        time.sleep(0.1)
+    return
 
 def on_clicked(icon, item):
     global run, log, plot
@@ -50,6 +51,12 @@ def on_clicked(icon, item):
     elif str(item) == "Logs":
         log = True
 
+def create_tray_image(text):
+    image = Image.new('RGBA', (64, 64), (255, 0, 0, 0))
+    dc = ImageDraw.Draw(image)
+    dc.text((0,0), text, font=ImageFont.truetype("arial.ttf", 60), fill=(255, 255, 255))
+    return image
+
 def update(icon, old_battery_percent):
     battery_percent = psutil.sensors_battery().percent
     if (battery_percent != old_battery_percent):
@@ -59,7 +66,7 @@ def update(icon, old_battery_percent):
         return battery_percent
     return old_battery_percent
 
-def log_data(percent):
+def log_data(percent, razer = None):
     if os.path.exists('Log.json'):
         DAY = 86400
         with open("Log.json") as file:
@@ -69,13 +76,18 @@ def log_data(percent):
                 data["time"].append(int(time.time()))
             else:
                 data["time"][-1] = int(time.time())
+
+            # Test only
+            if razer:
+                data["razer"].append(razer)
             
             while (data["time"][-1] - data["time"][0]) > (DAY * 7):
                 data["time"] = data["time"][1:]
                 data["percent"] = data["percent"][1:]
     else:
         data = {"percent" : [percent],
-                "time" : [int(time.time())]}
+                "time" : [int(time.time())],
+                "razer" : []}
     
     with open("Log.json", "w") as file:
         json.dump(data, file)
---
+++ Razer.py
@@ -0,0 +1,85 @@
+import time
+import logging
+import usb.core
+import usb.util
+from usb.backend import libusb1
+
+# declare constants
+# 1. product ID
+# see README.md for instruction to find the device ID for your mouse
+WIRELESS_RECEIVER = 0x00AB
+WIRED_MOUSE = 0x00AA
+# 2. transaction_id.id
+# see README.md for instruction to find the correct transaction_id.id for your mouse
+TRAN_ID = b"\x1f"
+
+
+def get_mouse():
+    # declare backend: libusb1.0
+    backend = libusb1.get_backend()
+    # find the mouse by PyUSB
+    mouse = usb.core.find(idVendor=0x1532, idProduct=WIRELESS_RECEIVER, backend=backend)
+    # if the receiver is not found, mouse would be None
+    if not mouse:
+        # try finding the wired mouse
+        mouse = usb.core.find(idVendor=0x1532, idProduct=WIRED_MOUSE, backend=backend)
+        # still not found, then the mouse is not plugged in, log error
+        if not mouse:
+            logging.info(f"The specified mouse (PID:{WIRELESS_RECEIVER} or {WIRED_MOUSE}) cannot be found.")
+    
+    return mouse
+
+
+def battery_msg():
+    # adapted from https://github.com/rsmith-nl/scripts/blob/main/set-ornata-chroma-rgb.py
+    # the first 8 bytes in order from left to right
+    # status + transaction_id.id + remaining packets (\x00\x00) + protocol_type + command_class + command_id + data_size
+    msg = b"\x00" + TRAN_ID + b"\x00\x00\x00\x02\x07\x80"
+    crc = 0
+    for i in msg[2:]:
+        crc ^= i
+    # the next 80 bytes would be storing the data to be sent, but for getting the battery no data is sent
+    msg += bytes(80)
+    # the last 2 bytes would be the crc and a zero byte
+    msg += bytes([crc, 0])
+    return msg
+
+
+def get_battery():
+    try:
+        # find the mouse and the state, see get_mouse() for detail
+        mouse = get_mouse()
+        if not mouse:
+            return None
+        # the message to be sent to the mouse, see battery_msg() for detail
+        msg = battery_msg()
+        logging.info(f"Message sent to the mouse: {list(msg)}")
+        # needed by PyUSB
+        # if Linux, need to detach kernel driver
+        mouse.set_configuration()
+        usb.util.claim_interface(mouse, 0)
+        # send request (battery), see razer_send_control_msg in razercommon.c in OpenRazer driver for detail
+        mouse.ctrl_transfer(bmRequestType=0x21, bRequest=0x09, wValue=0x300, data_or_wLength=msg, wIndex=0x00)
+        # needed by PyUSB
+        usb.util.dispose_resources(mouse)
+        # if the mouse is wireless, need to wait before getting response
+        time.sleep(0.3305)
+        # receive response
+        result = mouse.ctrl_transfer(bmRequestType=0xa1, bRequest=0x01, wValue=0x300, data_or_wLength=90, wIndex=0x00)
+        usb.util.dispose_resources(mouse)
+        usb.util.release_interface(mouse, 0)
+        logging.info(f"Message received from the mouse: {list(result)}")
+        # the raw battery level is in 0 - 255, scale it to 100 for human, correct to 2 decimal places
+    except Exception as e:
+        logging.info(e)
+        return None
+    
+    if (result[-2] == 122):
+        return f"{result[9] / 255 * 100:.0f}"
+    else:
+        return None
+
+if __name__ == "__main__":
+    battery = get_battery()
+    logging.info(f"Battery level obtained: {battery}")
+    logging.info(battery)
