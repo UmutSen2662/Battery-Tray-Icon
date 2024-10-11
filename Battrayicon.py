addd92ddd8c79c5acb164b1fb7776e7ddfcd8505
Umut Şen <umutsen2662@gmail.com>
2024-10-11 22:59:21 +0300

    Changed the font and added colors to show mouse battery state

--- Battrayicon.py
+++ Battrayicon.py
@@ -1,11 +1,12 @@
+import psutil, time, json, os, Logs, Plot, Razer, sys
 from PIL import Image, ImageDraw, ImageFont
-import psutil, time, json, os, Logs, Plot, Razer
 import pystray as ps
 
 FQ = 600
 run = True
 log = False
 plot = False
+path = sys._MEIPASS if hasattr(sys, '_MEIPASS') else '.'
 
 def main():
     battery_percent = psutil.sensors_battery().percent
@@ -15,9 +16,10 @@ def main():
                                 ps.MenuItem("Logs", on_clicked), 
                                 ps.MenuItem("Exit", on_clicked)))
     icon.run_detached()
-
+    
     repetitions = FQ
-    razer_battery = 0
+    razer_battery = 50
+    razer_battery_notification = "normal"
     global run, log, plot
     while (run):
         repetitions += 1
@@ -25,14 +27,24 @@ def main():
             new_razer_battery = Razer.get_battery()
             if new_razer_battery:
                 icon.title = f"Mouse {new_razer_battery}%"
-                if razer_battery - new_razer_battery > 0:
-                    if new_razer_battery < 25:
+                if new_razer_battery < 25:
+                    if razer_battery - new_razer_battery > 0:
                         icon.notify("Razer mouse battery is low plug in to charge", f"{new_razer_battery}% battery left")
-                    razer_battery = new_razer_battery
-                elif new_razer_battery - razer_battery > 0:
-                    if new_razer_battery > 75:
+                        razer_battery_notification = "low"
+                    elif razer_battery - new_razer_battery < 0:
+                        icon.remove_notification()
+                        razer_battery_notification = "normal"
+                elif new_razer_battery > 75:
+                    if new_razer_battery - razer_battery > 0:
                         icon.notify("Razer mouse battery is sufficiently charged", f"Charged to {new_razer_battery}%")
-                    razer_battery = new_razer_battery
+                        razer_battery_notification = "high"
+                    elif new_razer_battery - razer_battery < 0:
+                        icon.remove_notification()
+                        razer_battery_notification = "normal"
+                else:
+                    icon.remove_notification()
+                    razer_battery_notification = "normal"
+                razer_battery = new_razer_battery
             repetitions = 0
 
         if plot:
@@ -42,7 +54,7 @@ def main():
             Logs.show_logs()
             log = False
         elif (repetitions % 50 == 0):
-            battery_percent = update(icon, battery_percent)
+            battery_percent = update(icon, battery_percent, razer_battery_notification)
 
         time.sleep(0.1)
     return
@@ -57,17 +69,18 @@ def on_clicked(icon, item):
     elif str(item) == "Logs":
         log = True
 
-def create_tray_image(text):
-    image = Image.new('RGBA', (64, 64), (255, 0, 0, 0))
+def create_tray_image(text, razer_battery_notification = "normal"):
+    fill_color = (255, 0, 0) if razer_battery_notification == "low" else (0, 255, 0) if razer_battery_notification == "high" else (255, 255, 255)
+    image = Image.new('RGBA', (64, 64), (0, 0, 0, 0))
     dc = ImageDraw.Draw(image)
-    dc.text((0,0), text, font=ImageFont.truetype("arial.ttf", 60), fill=(255, 255, 255))
+    dc.text((0,0), text, font=ImageFont.truetype(os.path.join(path, "RobotoCondensed-Medium.ttf"), 64), fill=fill_color)
     return image
 
-def update(icon, old_battery_percent):
+def update(icon, old_battery_percent, razer_battery_notification):
     battery_percent = psutil.sensors_battery().percent
+    print(battery_percent)
+    icon.icon = create_tray_image(str(battery_percent), razer_battery_notification)
     if (battery_percent != old_battery_percent):
-        print(battery_percent)
-        icon.icon = create_tray_image(str(battery_percent))
         log_data(battery_percent)
         return battery_percent
     return old_battery_percent
